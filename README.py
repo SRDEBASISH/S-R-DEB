334c44e1d3ac3d4181d029e0e531e8c74c199742
SRDEBASISH <64908617+SRDEBASISH@users.noreply.github.com>
2020-05-07 20:04:53 +0530

    Update and rename README.md to README.py

---
+++ README.py
@@ -0,0 +1,18 @@
+# S-R-DEB
+a = 0
+b = 1
+x = int(input("Enter the no. of terms :"))
+print(a)
+print(",")
+print(b)
+print(",")
+i = 0
+while(i<x)
+  c = a+b
+  print(c)
+  print(",")
+  a = b
+  b = c
+  i = i+1
+  
+
